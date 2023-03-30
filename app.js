7e309cc28eca5a0630b11c26364d5421e16323a4
fuckrfromqc <120679133+fuckrfromqc@users.noreply.github.com>
2023-03-30 15:35:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,25 +10,28 @@ document.getElementById('instaForm').addEventListener('submit', async (event) =>
         return;
     }
 
-    const instagramData = await getInstagramData(username);
-    const followers = exportFollowers ? instagramData.followers : [];
-    const following = exportFollowing ? instagramData.following : [];
+    try {
+        const instagramData = await getInstagramData(username);
+        const followers = exportFollowers ? instagramData.followers : [];
+        const following = exportFollowing ? instagramData.following : [];
 
-    const csvContent = convertToCSV(followers, following);
-    downloadCSV(csvContent, `${username}_INSTACSV.csv`);
+        const csvContent = convertToCSV(followers, following);
+        downloadCSV(csvContent, `${username}_INSTACSV.csv`);
+    } catch (error) {
+        alert("An error occurred while fetching the data. Please make sure the Instagram account is public and the username is correct.");
+    }
 });
 
 async function getInstagramData(username) {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const url = `${proxyUrl}https://www.instagram.com/${username}/?__a=1`;
-    const response = await axios.get(url);
+    const proxyUrl = 'https://api.allorigins.win/raw?url=';
+    const targetUrl = `https://www.instagram.com/${username}/?__a=1`;
+    const response = await axios.get(proxyUrl + targetUrl);
     const data = response.data.graphql.user;
     const followers = data.edge_followed_by.edges.map(edge => edge.node.username);
     const following = data.edge_follow.edges.map(edge => edge.node.username);
 
     return { followers, following };
 }
-
 
 function convertToCSV(followers, following) {
     const maxRows = Math.max(followers.length, following.length);
